022b8be1fcb657be53ebe4a122e6ff8f39c85460
DAHameedi32 <daniyal.hameedi@kcl.ac.uk>
2024-03-02 23:08:44 +0000

    worked on projectile manager, so that now only one projectile is emitted at a time

--- Assets/Projectile_Manager.cs
+++ Assets/Projectile_Manager.cs
@@ -6,27 +6,32 @@ public class Projectile_Manager : MonoBehaviour
 {
     //projectile variables:
     public float mass;
-    public float velocity;
+    public float velocity; //escape velocity
     public float thrust;
     public GameObject point_of_origin;
     public GameObject projectile;
+    public bool isFired = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFired = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         //if input is pressed fire a projectile from the origin point with velocity
-        if(Input.GetKey("space"))
+        if(isFired == true)
         {
             GameObject projectile_instance = Instantiate(projectile, point_of_origin.GetComponent<Transform>().position,
                                                          point_of_origin.GetComponent<Transform>().rotation);
             projectile_instance.tag = "celestial_body";
             projectile_instance.GetComponent<Rigidbody>().mass = mass;
+            //need to get the force from escape velocity. hmmmm.
+
             projectile_instance.GetComponent<Rigidbody>().AddForce(Vector3.forward * thrust);
+            isFired = false;
         }
     }
 }
